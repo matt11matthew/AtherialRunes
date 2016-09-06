52f86384b0235496a9ea00a77640ac78a28eb82d
VawkeIO <io.vawke@gmail.com>
2016-09-06 16:41:41 +0200

    Data Registries / Annotations / Maven

--- AtherialRunes-game/src/main/java/me/matt11matthew/atherialrunes/game/enums/MessageType.java
+++ AtherialRunes-game/src/main/java/me/matt11matthew/atherialrunes/game/enums/MessageType.java
@@ -2,7 +2,7 @@ package me.matt11matthew.atherialrunes.game.enums;
 
 import org.bukkit.entity.Player;
 
-import de.Herbystar.TTA.TTA_Methods;
+//import de.Herbystar.TTA.TTA_Methods;
 import me.matt11matthew.atherialrunes.utils.Utils;
 
 public enum MessageType {
@@ -19,19 +19,19 @@ public enum MessageType {
 		msg = msg.replaceAll("%TAB%", "    ");
 		switch (this) {
 			case ACTION:
-				TTA_Methods.sendActionBar(player, Utils.colorCodes(msg));
+				//TTA_Methods.sendActionBar(player, Utils.colorCodes(msg));
 				break;
 			case CHAT:
 				player.sendMessage(Utils.colorCodes(msg));
 				break;
 			case SUB_TITLE:
-				TTA_Methods.sendTitle(player, "", 20, 20, 20, Utils.colorCodes(msg), 20, 20, 20);
+				//TTA_Methods.sendTitle(player, "", 20, 20, 20, Utils.colorCodes(msg), 20, 20, 20);
 				break;
 			case TITLE:
-				TTA_Methods.sendTitle(player, Utils.colorCodes(msg), 20, 20, 20, "", 20, 20, 20);
+				//TTA_Methods.sendTitle(player, Utils.colorCodes(msg), 20, 20, 20, "", 20, 20, 20);
 				break;
 			case TITLE_SUB_TITLE:
-				TTA_Methods.sendTitle(player, Utils.colorCodes(msg.split("SUB:")[0].trim()), 20, 20, 20, Utils.colorCodes(msg.split("SUB:")[1].trim()), 20, 20, 20);
+				//TTA_Methods.sendTitle(player, Utils.colorCodes(msg.split("SUB:")[0].trim()), 20, 20, 20, Utils.colorCodes(msg.split("SUB:")[1].trim()), 20, 20, 20);
 				break;
 			default:
 				break;
