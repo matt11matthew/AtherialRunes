eda66251836c823862f7bb59898fd64b03e8fcea
Matthew <matt11matthew@gmail.com>
2016-09-06 16:30:31 -0400

    Custom Items JSONUtils

---
+++ AtherialRunes-common/src/main/java/me/matt11matthew/atherialrunes/utils/FileUtils.java
@@ -0,0 +1,46 @@
+package me.matt11matthew.atherialrunes.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileUtils {
+
+    public static String getTextOfFile(String path, String name) {
+        File file = new File(path);
+        String text = null;
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        file = new File(path, name);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            text = sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return text;
+    }
+}
---
+++ AtherialRunes-game/src/main/java/me/matt11matthew/atherialrunes/game/player/PlayerStatistics.java
@@ -0,0 +1,15 @@
+package me.matt11matthew.atherialrunes.game.player;
+
+public class PlayerStatistics {
+
+    private final GamePlayer gamePlayer;
+
+    public PlayerStatistics(GamePlayer gamePlayer) {
+        this.gamePlayer = gamePlayer;
+    }
+
+    public int t1mobKills, t2mobKills, t3mobKills, t4mobKills;
+
+    public int playerKills;
+
+}
